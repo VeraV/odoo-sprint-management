20549f5eb56988a91633cea99b47f593ceead489
Vera Fileyeva <veremei.vera@gmail.com>
2026-01-30 15:02:39 +0000

    changes from main

--- addons/sprint_management/models/project_task.py
+++ addons/sprint_management/models/project_task.py
@@ -1,6 +1,32 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     sprint_id = fields.Many2one('project.sprint', string='Sprint', ondelete='set null')
+
+    #@api.onchange('project_id')
+    #def _onchange_project_id_sprint_warning(self):
+    #    """Warn immediately and revert when trying to change project on a task in a sprint."""
+    #    if self._origin.id and self.sprint_id and self.project_id != self.sprint_id.project_id:
+    #        sprint_name = self.sprint_id.name
+    #        self.project_id = self.sprint_id.project_id  # Revert to original
+    #        return {
+    #            'warning': {
+    #                'title': 'Cannot Change Project',
+    #                'message': f'Remove the task from sprint "{sprint_name}" first before changing the project.'
+    #            }
+    #        }
+
+    def write(self, vals):
+        """Block project change if task is in a sprint (backup validation)."""
+        if 'project_id' in vals:
+            for task in self:
+                if task.sprint_id and vals['project_id'] != task.sprint_id.project_id.id:
+                    raise UserError(
+                        f"Cannot change project while task is in a sprint. "
+                        f"Remove the task from sprint '{task.sprint_id.name}' first."
+                    )
+        return super().write(vals)
--- addons/sprint_management/models/sprint.py
+++ addons/sprint_management/models/sprint.py
@@ -1,10 +1,9 @@
 from odoo import models, fields, api
 
 class ProjectSprint(models.Model):
-    #Odoo model attributes
-    _name = 'project.sprint'#creates table project_sprint, required
-    _description = 'Project Sprint' #shows in logs, good for docs
-    _order = 'start_date desc'#SQL: ORDER BY start_date DESC
+    _name = 'project.sprint'
+    _description = 'Project Sprint'
+    _order = 'start_date desc'
 
     name = fields.Char(string='Sprint Name', required=True)
     project_id = fields.Many2one('project.project', string='Project', required=True, ondelete='cascade')
@@ -19,8 +18,47 @@ class ProjectSprint(models.Model):
     goal = fields.Text(string='Sprint Goal')
     task_ids = fields.One2many('project.task', 'sprint_id', string='Tasks')
     task_count = fields.Integer(string='Task Count', compute='_compute_task_count')
+    available_task_ids = fields.Many2many(
+        'project.task',
+        'sprint_task_add_rel',
+        'sprint_id',
+        'task_id',
+        string='Add Existing Tasks',
+    )
 
-    @api.depends('task_ids')#mark as compute method, call each time task_ids changes
+    @api.depends('task_ids')
     def _compute_task_count(self):
         for sprint in self:
             sprint.task_count = len(sprint.task_ids)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        sprints = super().create(vals_list)
+        sprints._assign_available_tasks()
+        # sprints._validate_task_projects()  # Temporarily disabled for debugging
+        return sprints
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'available_task_ids' in vals:
+            self._assign_available_tasks()
+        return res
+
+    def _assign_available_tasks(self):
+        """Assign tasks from available_task_ids to this sprint, then clear picker."""
+        for sprint in self:
+            if sprint.available_task_ids:
+                sprint.available_task_ids.write({'sprint_id': sprint.id})
+                sprint.available_task_ids = [(5, 0, 0)]
+
+    @api.onchange('project_id')
+    def _onchange_project_id(self):
+        """Prevent changing project if sprint has tasks."""
+        if self._origin.project_id and self.task_ids:
+            self.project_id = self._origin.project_id
+            return {
+                'warning': {
+                    'title': 'Cannot Change Project',
+                    'message': 'Remove all tasks first before changing the project.'
+                }
+            }
